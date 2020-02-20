fdc1cdc249888475b3d7b6cd0935c2f23cec4d44
Franco Zaccra <francozaccra@hotmail.com>
2020-02-20 17:19:43 -0300

    INIATIAL COMMIT

---
+++ bootloader/bootloaderYTX/Bootloader/ArduinoCore/library.cpp
@@ -0,0 +1,15 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 17/2/2020 10:49:57 a. m.
+ * Author : Franco
+ */ 
+
+#include "sam.h"
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
