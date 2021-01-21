723cbfa7de6c1b8836bfa3666064c7d05f247e6e
SanoYTX <franco@yaeltex.com>
2021-01-21 17:25:38 -0300

    Send MIDI clock as master. Grid note selection. Tap tempo on BPM button.

--- ytx-controller/headers/sequencer.h
+++ ytx-controller/headers/sequencer.h
@@ -35,6 +35,7 @@ const uint8_t ListMasterSpeed[] = {100, 75, 50, 40, 30, 15, 10, 5};
 
 #define NUM_SEQUENCERS					    1
 #define SEQ_MAX_STEPS       			  16
+#define GRID_HEIGHT                 8
 #define MS_CLOCK                    25
 #define MASTER_SPEED                1
 #define SEQ_MAX_SPEED      				  8
@@ -57,6 +58,7 @@ const uint8_t ListMasterSpeed[] = {100, 75, 50, 40, 30, 15, 10, 5};
 #define MASTER_SLAVE_BUTTON   64
 #define PLAY_STOP_BUTTON      65
 #define MODIFY_ALL_BUTTON     72
+#define BPM_BUTTON            79
 
 #define NOTE_ENCODER          0
 #define OCTAVE_ENCODER        1
@@ -73,7 +75,7 @@ const uint8_t ListMasterSpeed[] = {100, 75, 50, 40, 30, 15, 10, 5};
 #define STEP_ON_COLOR         64
 #define CURRENT_STEP_COLOR    127
 
-const byte scales[8][8] = { {0,   2,  4,  5,  7,  9,  11, 12},  // MAJOR
+const byte scales[8][8] = { {0,   1,  2,  3,  4,  5,  6, 7},  // MAJOR
                             {0,   2,  3,  5,  7,  8,  10, 12},  // MINOR
                             {2,   4,  5,  7,  9,  11, 12, 14},  // DORIAN
                             {4,   5,  7,  9,  11, 12, 14, 16},  // PHRYGIAN
@@ -83,22 +85,22 @@ const byte scales[8][8] = { {0,   2,  4,  5,  7,  9,  11, 12},  // MAJOR
                             {11,  12, 14, 16, 17, 19, 21, 23}}; // LOCRIAN
 
 
-const uint8_t StepButtons[SEQ_MAX_STEPS][8] = { {0, 8, 16, 24, 32, 40, 48, 56}, 
-                                                {1, 9, 17, 25, 33, 41, 49, 57}, 
-                                                {2, 10, 18, 26, 34, 42, 50, 58},
-                                                {3, 11, 19, 27, 35, 43, 51, 59},
-                                                {4, 12, 20, 28, 36, 44, 52, 60},
-                                                {5, 13, 21, 29, 37, 45, 53, 61},
-                                                {6, 14, 22, 30, 38, 46, 54, 62},
-                                                {7, 15, 23, 31, 39, 47, 55, 63},
-                                                {80, 88, 96, 104, 112, 120, 128, 136},
-                                                {81, 89, 97, 105, 113, 121, 129, 137},
-                                                {82, 90, 98, 106, 114, 122, 130, 138},
-                                                {83, 91, 99, 107, 115, 123, 131, 139},
-                                                {84, 92, 100, 108, 116, 124, 132, 140},
-                                                {85, 93, 101, 109, 117, 125, 133, 141},
-                                                {86, 94, 102, 110, 118, 126, 134, 142},
-                                                {87, 95, 103, 111, 119, 127, 135, 143}};
+const uint8_t StepButtons[SEQ_MAX_STEPS][GRID_HEIGHT] = { {0,   8,  16,   24,   32,   40,   48,   56}, 
+                                                          {1,   9,  17,   25,   33,   41,   49,   57}, 
+                                                          {2,   10, 18,   26,   34,   42,   50,   58},
+                                                          {3,   11, 19,   27,   35,   43,   51,   59},
+                                                          {4,   12, 20,   28,   36,   44,   52,   60},
+                                                          {5,   13, 21,   29,   37,   45,   53,   61},
+                                                          {6,   14, 22,   30,   38,   46,   54,   62},
+                                                          {7,   15, 23,   31,   39,   47,   55,   63},
+                                                          {80,  88, 96,   104,  112,  120,  128,  136},
+                                                          {81,  89, 97,   105,  113,  121,  129,  137},
+                                                          {82,  90, 98,   106,  114,  122,  130,  138},
+                                                          {83,  91, 99,   107,  115,  123,  131,  139},
+                                                          {84,  92, 100,  108,  116,  124,  132,  140},
+                                                          {85,  93, 101,  109,  117,  125,  133,  141},
+                                                          {86,  94, 102,  110,  118,  126,  134,  142},
+                                                          {87,  95, 103,  111,  119,  127,  135,  143}};
 
 const uint8_t Modifiers[SEQ_MAX_STEPS] = {8, 9, 10, 11, 12, 13, 14, 15, 88, 89, 90, 91, 92, 93, 94, 95};
 
@@ -108,6 +110,7 @@ bool sequencerOnPrevButtonState = 0;
 
 uint8_t currentSequencer = 0;
 uint32_t antMicrosSequencerInterval = 0;
+uint8_t clockCounter = 0;
 
 typedef struct __attribute__((packed)){
   uint8_t currentNote;
@@ -117,25 +120,28 @@ typedef struct __attribute__((packed)){
   uint8_t currentScale:4;
   uint8_t notePlaying;
   uint8_t channelPlaying;
-  uint8_t stepButtonPrevState:1;
+  uint8_t stepButtonPrevState;
   uint8_t modifierButtonPrevState:1;
   uint8_t settingUp:1;
-  uint8_t unused:5;
+  uint8_t unused:6;
   unsigned long offMillis;
 }stepSettings;
 
 typedef struct __attribute__((packed)){
-  uint16_t stateOfSteps;
+  uint16_t stateOfSteps[GRID_HEIGHT];
   uint16_t tempoTimer;
 	uint8_t currentStep;
 	uint8_t currentSpeed;
+  float currentBPM;
+  uint32_t antMicrosBPM;
   stepSettings steps[SEQ_MAX_STEPS];
 	uint8_t sequencerPlaying:1;
   uint8_t playButtonPrevState:1;
 	uint8_t masterMode:1;
   uint8_t masterModeButtonPrevState:1;
   uint8_t modifyAll:1;
-  uint8_t unused:4;
+  uint8_t bpmButtonPrevState:1;
+  uint8_t unused:2;
 	uint8_t firstStep;
   uint8_t finalStep;
 }seqSettingsStruct;
--- ytx-controller/sequencer.ino
+++ ytx-controller/sequencer.ino
@@ -31,12 +31,14 @@ SOFTWARE.
 void InitSequencer(void) {
 	currentSequencer = 0;
 	for(int seq = 0; seq < NUM_SEQUENCERS; seq++){
-		seqSettings[seq].stateOfSteps   = 0;
+    for(int h = 0; h < GRID_HEIGHT; h++){
+      seqSettings[seq].stateOfSteps[h]   = 0;
+    }
 		seqSettings[seq].tempoTimer     = 0;
     seqSettings[seq].currentStep    = 0;
     for(int step = 0; step < SEQ_MAX_STEPS; step++){
       seqSettings[seq].steps[step].offMillis = 0;
-      seqSettings[seq].steps[step].stepButtonPrevState      = false;
+      seqSettings[seq].steps[step].stepButtonPrevState      = 0;
       seqSettings[seq].steps[step].modifierButtonPrevState  = false;
       seqSettings[seq].steps[step].settingUp                = false;
       seqSettings[seq].steps[step].notePlaying      = 0;
@@ -49,6 +51,7 @@ void InitSequencer(void) {
     
     } 
     seqSettings[seq].currentSpeed               = 4; 
+    seqSettings[seq].currentBPM                 = 60; 
     seqSettings[seq].sequencerPlaying           = true; 
     seqSettings[seq].masterMode                 = true;   
     seqSettings[seq].playButtonPrevState        = false; 
@@ -95,14 +98,23 @@ void UpdateSequencer(void) {
 
     for(int seq = 0; seq < NUM_SEQUENCERS; seq++){
       if (seqSettings[seq].masterMode && seqSettings[currentSequencer].sequencerPlaying) {
-        if (micros() - antMicrosSequencerInterval > SEQUENCER_MIN_TIME_INTERVAL) {
+        if (micros() - antMicrosSequencerInterval > (60*1000000/(seqSettings[currentSequencer].currentBPM*24))) {
+          // SerialUSB.print("Time elapsed: "); SerialUSB.println(micros() - antMicrosSequencerInterval);
+          antMicrosSequencerInterval = micros();
+          clockCounter++;
+          clockCounter %= 24; 
+          MIDI.sendRealTime(midi::Clock);
+          if(!clockCounter)
+            feedbackHw.SetChangeDigitalFeedback(BPM_BUTTON, 127, true,  NO_SHIFTER, NO_BANK_UPDATE);
+          else if(clockCounter == 12)
+            feedbackHw.SetChangeDigitalFeedback(BPM_BUTTON, 127, false, NO_SHIFTER, NO_BANK_UPDATE);
+
           // if (seqSettings[seq].tempoTimer++ >= pow((8-seqSettings[seq].currentSpeed),2)*MASTER_SPEED) {
           seqSettings[seq].tempoTimer++;
-          seqSettings[seq].tempoTimer %= ListMasterSpeed[seqSettings[seq].currentSpeed];
+          seqSettings[seq].tempoTimer %= ListClockMenu[seqSettings[seq].currentSpeed];
           if(!seqSettings[seq].tempoTimer) {		// SEQ SPEED
             SeqNextStep(seq);
           }
-          antMicrosSequencerInterval = micros();
         }
       }
     }
@@ -116,68 +128,94 @@ void UpdateSequencer(void) {
 
 void SeqNextStep(int seq) {
   uint8_t prevStep = seqSettings[seq].currentStep;
-  bool prevStepState = bitRead(seqSettings[seq].stateOfSteps, seqSettings[seq].currentStep);
+  bool prevStepState[GRID_HEIGHT] = {0};
 
+  for(int h = 0; h < GRID_HEIGHT; h++){
+    prevStepState[h] = bitRead(seqSettings[seq].stateOfSteps[h], seqSettings[seq].currentStep);    
+  }
   // GET NEXT STEP
   seqSettings[seq].currentStep++;
   seqSettings[seq].currentStep %= seqSettings[seq].finalStep;
 
   // SerialUSB.print("Step: "); SerialUSB.print(seqSettings[seq].currentStep);
-  bool currentStepState = bitRead(seqSettings[seq].stateOfSteps, seqSettings[seq].currentStep);
-  // SerialUSB.print("\tState: "); SerialUSB.println(currentStepState);
-  //if (!setupMode){
+  bool currentStepState[GRID_HEIGHT] = {0};
+  for(int h = 0; h < GRID_HEIGHT; h++){
+    currentStepState[h] = bitRead(seqSettings[seq].stateOfSteps[h], seqSettings[seq].currentStep);
     // TURN LED FROM CURRENT STEP ON
-  feedbackHw.SetChangeDigitalFeedback(StepButtons[prevStep][0],                      STEP_ON_COLOR,      prevStepState,  NO_SHIFTER, NO_BANK_UPDATE);
-  for(int i = 1; i < 8; i++){
-    feedbackHw.SetChangeDigitalFeedback(StepButtons[prevStep][i],                      STEP_ON_COLOR,      false,  NO_SHIFTER, NO_BANK_UPDATE);
-  }
-  for(int i = 0; i < 8; i++){
-	  feedbackHw.SetChangeDigitalFeedback(StepButtons[seqSettings[seq].currentStep][i],  CURRENT_STEP_COLOR, true, 				  NO_SHIFTER, NO_BANK_UPDATE);
-  }
-  // }
-	
+    feedbackHw.SetChangeDigitalFeedback(StepButtons[prevStep][h],                      STEP_ON_COLOR,      prevStepState[h],  NO_SHIFTER, NO_BANK_UPDATE);
+    feedbackHw.SetChangeDigitalFeedback(StepButtons[seqSettings[seq].currentStep][h],  CURRENT_STEP_COLOR, true, 				      NO_SHIFTER, NO_BANK_UPDATE);
+  
   // SEND NOTE ON FOR CURRENT STEP  	
-  if (currentStepState){
-    SendStepNote(seq, seqSettings[seq].currentStep, NOTE_ON);
-    // SerialUSB.println("NOTE SENT");
-  } 
+    if (currentStepState[h]){
+      SendStepNote(seq, seqSettings[seq].currentStep, NOTE_ON, GRID_HEIGHT-1-h);
+      // SerialUSB.println("NOTE SENT");
+    } 
+  }
 }
 
 void CheckUserInput(void){
 	// GLOBAL BUTTONS
-  bool masterModeButtonState = digitalHw.GetDigitalState(64);
+  bool masterModeButtonState = digitalHw.GetDigitalState(MASTER_SLAVE_BUTTON);
   if(masterModeButtonState != seqSettings[currentSequencer].masterModeButtonPrevState){
     seqSettings[currentSequencer].masterModeButtonPrevState = masterModeButtonState;
     if(masterModeButtonState){
       seqSettings[currentSequencer].masterMode = !seqSettings[currentSequencer].masterMode;
       if(seqSettings[currentSequencer].masterMode){
-        feedbackHw.SetChangeDigitalFeedback(64, MASTER_MODE_COLOR,  true,   NO_SHIFTER, NO_BANK_UPDATE);
+        feedbackHw.SetChangeDigitalFeedback(MASTER_SLAVE_BUTTON, MASTER_MODE_COLOR,  true,   NO_SHIFTER, NO_BANK_UPDATE);
       }else{
-        feedbackHw.SetChangeDigitalFeedback(64, SLAVE_MODE_COLOR,   true,   NO_SHIFTER, NO_BANK_UPDATE);
+        feedbackHw.SetChangeDigitalFeedback(MASTER_SLAVE_BUTTON, SLAVE_MODE_COLOR,   true,   NO_SHIFTER, NO_BANK_UPDATE);
       }
     }
   }
-  bool playButtonState = digitalHw.GetDigitalState(65);
+  bool playButtonState = digitalHw.GetDigitalState(PLAY_STOP_BUTTON);
   if(playButtonState != seqSettings[currentSequencer].playButtonPrevState){
     seqSettings[currentSequencer].playButtonPrevState = playButtonState;
     if(playButtonState){
       seqSettings[currentSequencer].sequencerPlaying = !seqSettings[currentSequencer].sequencerPlaying;
       seqSettings[currentSequencer].tempoTimer = 0;
+      clockCounter = 0;
       if(seqSettings[currentSequencer].sequencerPlaying){
-        feedbackHw.SetChangeDigitalFeedback(65, PLAY_COLOR, true,   NO_SHIFTER, NO_BANK_UPDATE);
+
+        feedbackHw.SetChangeDigitalFeedback(PLAY_STOP_BUTTON, PLAY_COLOR, true,   NO_SHIFTER, NO_BANK_UPDATE);
       }else{
-        feedbackHw.SetChangeDigitalFeedback(65, STOP_COLOR, true,   NO_SHIFTER, NO_BANK_UPDATE);
+        feedbackHw.SetChangeDigitalFeedback(PLAY_STOP_BUTTON, STOP_COLOR, true,   NO_SHIFTER, NO_BANK_UPDATE);
       }
     }
   }
 
-  bool modifyAllButtonState = digitalHw.GetDigitalState(72);
+  static bool tapTempoOn = false;
+  if(seqSettings[currentSequencer].masterMode){
+    bool bpmButtonState = digitalHw.GetDigitalState(BPM_BUTTON);
+    if(bpmButtonState != seqSettings[currentSequencer].bpmButtonPrevState){
+      seqSettings[currentSequencer].bpmButtonPrevState = bpmButtonState;
+      if(bpmButtonState){
+        if(!tapTempoOn){
+          tapTempoOn = true;
+          seqSettings[currentSequencer].antMicrosBPM = micros();
+          SerialUSB.println("TAP ON");
+        }else{
+          float newBPM = (float) 60/((float) (micros()-seqSettings[currentSequencer].antMicrosBPM)/1000000);
+          seqSettings[currentSequencer].currentBPM += newBPM;
+          seqSettings[currentSequencer].currentBPM /= 2;
+          seqSettings[currentSequencer].antMicrosBPM = micros();
+          SerialUSB.print("Current BPM:"); SerialUSB.println(seqSettings[currentSequencer].currentBPM);
+        }
+      }
+    }
+    if(micros()-seqSettings[currentSequencer].antMicrosBPM > 5*(60*1000000/seqSettings[currentSequencer].currentBPM)){
+      tapTempoOn = false;
+      SerialUSB.println("TAP OFF");
+    }
+  }
+    
+
+  bool modifyAllButtonState = digitalHw.GetDigitalState(MODIFY_ALL_BUTTON);
   if(modifyAllButtonState){
     seqSettings[currentSequencer].modifyAll = true;
-    feedbackHw.SetChangeDigitalFeedback(72, 127, true,    NO_SHIFTER, NO_BANK_UPDATE);
+    feedbackHw.SetChangeDigitalFeedback(MODIFY_ALL_BUTTON, 127, true,    NO_SHIFTER, NO_BANK_UPDATE);
   } 
   else{
-    feedbackHw.SetChangeDigitalFeedback(72, 127, false,   NO_SHIFTER, NO_BANK_UPDATE);
+    feedbackHw.SetChangeDigitalFeedback(MODIFY_ALL_BUTTON, 127, false,   NO_SHIFTER, NO_BANK_UPDATE);
     seqSettings[currentSequencer].modifyAll = false;
   }                      
   
@@ -187,43 +225,46 @@ void CheckUserInput(void){
   bool currentStepState = 0;
   static unsigned long antMillisLongPress = 0;
 	for (int step = 0; step < SEQ_MAX_STEPS; step++){
-		stepOnButtonState = digitalHw.GetDigitalState(StepButtons[step][0]);
-		currentStepState = bitRead(seqSettings[currentSequencer].stateOfSteps, step);
-		if(stepOnButtonState != seqSettings[currentSequencer].steps[step].stepButtonPrevState){
-      if(stepOnButtonState){
-        bitWrite(seqSettings[currentSequencer].stateOfSteps, step, !currentStepState);
-        feedbackHw.SetChangeDigitalFeedback(StepButtons[step][0], STEP_ON_COLOR, !currentStepState,   NO_SHIFTER, NO_BANK_UPDATE);
+    for(int h = 0; h < GRID_HEIGHT; h++){
+      stepOnButtonState = digitalHw.GetDigitalState(StepButtons[step][h]);
+      currentStepState = bitRead(seqSettings[currentSequencer].stateOfSteps[h], step);
+      if(stepOnButtonState != bitRead(seqSettings[currentSequencer].steps[step].stepButtonPrevState, h)){
+        if(stepOnButtonState){
+          bitWrite(seqSettings[currentSequencer].stateOfSteps[h], step, !currentStepState);
+          feedbackHw.SetChangeDigitalFeedback(StepButtons[step][h], STEP_ON_COLOR, !currentStepState,   NO_SHIFTER, NO_BANK_UPDATE);
+        }
+        bitWrite(seqSettings[currentSequencer].steps[step].stepButtonPrevState, h, stepOnButtonState);
       }
-      seqSettings[currentSequencer].steps[step].stepButtonPrevState = stepOnButtonState;
-		}
+    }
+		
 
     // STEP MODIFICATORS
-    bool ModifierState = digitalHw.GetDigitalState(Modifiers[step]);
-    if(ModifierState != seqSettings[currentSequencer].steps[step].modifierButtonPrevState){
-      if(ModifierState && !seqSettings[currentSequencer].steps[step].settingUp){ 
-        seqSettings[currentSequencer].steps[step].settingUp = true;
-        feedbackHw.SetChangeDigitalFeedback(Modifiers[step], MODIFIER_COLOR, true,   NO_SHIFTER, NO_BANK_UPDATE);
-        encoderHw.SetEncoderValue(currentBank, NOTE_ENCODER,    seqSettings[currentSequencer].steps[step].currentNote);
-        encoderHw.SetEncoderValue(currentBank, OCTAVE_ENCODER,  seqSettings[currentSequencer].steps[step].currentOctave);
-        encoderHw.SetEncoderValue(currentBank, SCALE_ENCODER,   seqSettings[currentSequencer].steps[step].currentScale);
-        encoderHw.SetEncoderValue(currentBank, VELOCITY_ENCODER,seqSettings[currentSequencer].steps[step].currentVelocity);
-        encoderHw.SetEncoderValue(currentBank, DURATION_ENCODER,seqSettings[currentSequencer].steps[step].currentDuration);
-        SerialUSB.print(seqSettings[currentSequencer].steps[step].currentNote); SerialUSB.print("\t");
-        SerialUSB.print(seqSettings[currentSequencer].steps[step].currentOctave); SerialUSB.print("\t");
-        SerialUSB.print(seqSettings[currentSequencer].steps[step].currentScale); SerialUSB.print("\t");
-        SerialUSB.print(seqSettings[currentSequencer].steps[step].currentVelocity); SerialUSB.print("\t");
-        SerialUSB.print(seqSettings[currentSequencer].steps[step].currentDuration); SerialUSB.print("\n");
+    // bool ModifierState = digitalHw.GetDigitalState(Modifiers[step]);
+    // if(ModifierState != seqSettings[currentSequencer].steps[step].modifierButtonPrevState){
+    //   if(ModifierState && !seqSettings[currentSequencer].steps[step].settingUp){ 
+    //     seqSettings[currentSequencer].steps[step].settingUp = true;
+    //     feedbackHw.SetChangeDigitalFeedback(Modifiers[step], MODIFIER_COLOR, true,   NO_SHIFTER, NO_BANK_UPDATE);
+    //     encoderHw.SetEncoderValue(currentBank, NOTE_ENCODER,    seqSettings[currentSequencer].steps[step].currentNote);
+    //     encoderHw.SetEncoderValue(currentBank, OCTAVE_ENCODER,  seqSettings[currentSequencer].steps[step].currentOctave);
+    //     encoderHw.SetEncoderValue(currentBank, SCALE_ENCODER,   seqSettings[currentSequencer].steps[step].currentScale);
+    //     encoderHw.SetEncoderValue(currentBank, VELOCITY_ENCODER,seqSettings[currentSequencer].steps[step].currentVelocity);
+    //     encoderHw.SetEncoderValue(currentBank, DURATION_ENCODER,seqSettings[currentSequencer].steps[step].currentDuration);
+    //     SerialUSB.print(seqSettings[currentSequencer].steps[step].currentNote); SerialUSB.print("\t");
+    //     SerialUSB.print(seqSettings[currentSequencer].steps[step].currentOctave); SerialUSB.print("\t");
+    //     SerialUSB.print(seqSettings[currentSequencer].steps[step].currentScale); SerialUSB.print("\t");
+    //     SerialUSB.print(seqSettings[currentSequencer].steps[step].currentVelocity); SerialUSB.print("\t");
+    //     SerialUSB.print(seqSettings[currentSequencer].steps[step].currentDuration); SerialUSB.print("\n");
 
-        for(int step2 = 0; step2 < SEQ_MAX_STEPS; step2++){
-          if(step != step2){
-            seqSettings[currentSequencer].steps[step2].settingUp = false;
-            feedbackHw.SetChangeDigitalFeedback(Modifiers[step2], MODIFIER_COLOR, false,   NO_SHIFTER, NO_BANK_UPDATE);
-          }
-        }
-      }
-      seqSettings[currentSequencer].steps[step].modifierButtonPrevState = ModifierState;
+    //     for(int step2 = 0; step2 < SEQ_MAX_STEPS; step2++){
+    //       if(step != step2){
+    //         seqSettings[currentSequencer].steps[step2].settingUp = false;
+    //         feedbackHw.SetChangeDigitalFeedback(Modifiers[step2], MODIFIER_COLOR, false,   NO_SHIFTER, NO_BANK_UPDATE);
+    //       }
+    //     }
+    //   }
+    //   seqSettings[currentSequencer].steps[step].modifierButtonPrevState = ModifierState;
       
-    }
+    // }
 
     // ENCODERS
     if(seqSettings[currentSequencer].steps[step].settingUp || seqSettings[currentSequencer].modifyAll){      
@@ -237,8 +278,8 @@ void CheckUserInput(void){
   seqSettings[currentSequencer].currentSpeed = map(encoderHw.GetEncoderValue(SPEED_ENCODER), 0, 128, 0, SEQ_MAX_SPEED);
 }
 
-void SendStepNote(byte seq, byte step, byte noteOn) {
-  uint8_t stepNote      = map(seqSettings[seq].steps[step].currentNote, 0, 128, 0, 8);
+void SendStepNote(byte seq, byte step, byte noteOn, int height) {
+  uint8_t stepNote      = height;
   uint8_t stepOctave    = map(seqSettings[seq].steps[step].currentOctave, 0, 128, 0, 11);
   uint8_t stepScale     = map(seqSettings[seq].steps[step].currentScale, 0, 128, 0, 8);
   uint8_t stepVelocity  = seqSettings[seq].steps[step].currentVelocity;
@@ -258,6 +299,7 @@ void SendStepNote(byte seq, byte step, byte noteOn) {
 
   MIDI.sendNoteOn(noteToSend, stepVelocity, seq+1);
 }
+
 void SendNotesOff(void){
   // NOTES OFF
   for(int step = 0; step < SEQ_MAX_STEPS; step++){
