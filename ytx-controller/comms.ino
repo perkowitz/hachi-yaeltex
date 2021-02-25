28f74a5a6b307ef21a51af0f3ec5f58fc8746ee7
SanoYTX <franco@yaeltex.com>
2021-02-25 16:21:13 -0300

    Changed function name

--- ytx-controller/comms.ino
+++ ytx-controller/comms.ino
@@ -535,7 +535,7 @@ void ProcessMidi(byte msgType, byte channel, uint16_t param, int16_t value, bool
     value = 0;
   }else if(config->board.remoteBanks && msgType == midi::ProgramChange && channel == BANK_CHANGE_CHANNEL){    //   
     if(value < config->banks.count && config->banks.count > 1){
-      MidiBankChange(value);
+      ChangeToBank(value);
       bankUpdated = true;
     }
   }
