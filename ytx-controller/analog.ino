337d96ce3278bdce3c0c6f60cb81f52a67b09548
Franco Zaccra <francozaccra@hotmail.com>
2022-06-22 15:28:48 -0300

    More fine tweaking

--- ytx-controller/analog.ino
+++ ytx-controller/analog.ino
@@ -279,7 +279,7 @@ void AnalogInputs::Read(){
                 // Linearize mapping between adjacent table values
 
                 linearVal = constrain(linearVal,minRawValue,maxRawValue); 
-                
+
                 break;
               }
             }                 
